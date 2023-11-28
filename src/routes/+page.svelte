298f6dc42ca38b36020ee5374f67cad731d91029
takara2314 <takara2314_TK@outlook.jp>
2023-11-28 15:08:01 +0900

    Change: トップを近日公開っぽい表示にした

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,15 @@
 <main class="
-  w-[100dvw] min-h-[100dvh]
+  p-10 w-full h-full
   flex flex-col items-center justify-center
   overflow-x-hidden
 ">
-  <h1 class="text-5xl text-green-700 font-bold">
-    令和6年 志摩市二十歳の集い ウェブサイト
+  <div class="
+    pb-2 text-transparent text-8xl font-bold
+    bg-clip-text bg-gradient-to-r from-lime-500 to-lime-600
+  ">
+    Coming soon
+  </div>
+  <h1 class="mt-10 text-2xl">
+    令和6年 志摩市二十歳の集い
   </h1>
 </main>
-
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.50);
-  }
-</style>
