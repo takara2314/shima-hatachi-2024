4ef844d41f8b2c9a0469688d3ad6af69b9cb1dff
takara2314 <takara2314_TK@outlook.jp>
2023-11-28 14:47:30 +0900

    Include: Tailwind CSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="
+  w-[100dvw] min-h-[100dvh]
+  flex flex-col items-center justify-center
+  overflow-x-hidden
+">
+  <h1 class="text-5xl text-green-700 font-bold">
+    令和6年 志摩市二十歳の集い ウェブサイト
+  </h1>
+</main>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.50);
+  }
+</style>
