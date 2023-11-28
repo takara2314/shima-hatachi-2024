f36fc9055dee649dcd483c4fd41644e4daa6523e
takara2314 <takara2314_TK@outlook.jp>
2023-11-28 16:05:09 +0900

    Add: 404 Not Found ページ

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<main class="
+  p-10 w-full h-full
+  flex flex-col items-center justify-center
+  overflow-x-hidden
+">
+  <div class="
+    pb-2 text-transparent text-8xl font-bold
+    bg-clip-text bg-gradient-to-r from-red-400 to-red-600
+  ">
+    404 Not Found
+  </div>
+  <h1 class="mt-10 text-2xl">
+    このページは存在しません。
+  </h1>
+</main>
