3a7dd1e2f0a5b268d37a9c0f07f84d19782d2f77
takara2314 <takara2314_TK@outlook.jp>
2023-12-14 23:20:18 +0900

    Add: メタタグを充実させた

---
+++ src/components/MetaTags.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { MetaTags } from 'svelte-meta-tags';
+  import { PUBLIC_SITE_URL } from '$env/static/public';
+
+  const baseTitle = '令和6年度志摩市二十歳の集い';
+  export let title = baseTitle;
+  export let description: string;
+  export let href = '/';
+
+  $: metaTags = {
+    title: title,
+    titleTemplate: href !== '/' ? `${title} | ${baseTitle}` : baseTitle,
+    description: description,
+    canonical: `${PUBLIC_SITE_URL}${href}`,
+    openGraph: {
+      url: `${PUBLIC_SITE_URL}${href}`,
+      title: href !== '/' ? `${title} | ${baseTitle}` : baseTitle,
+      description: description,
+      images: [
+        {
+          url: `${PUBLIC_SITE_URL}/ogp.webp`,
+          width: 800,
+          height: 600,
+          alt: 'OGP'
+        }
+      ],
+      siteName: baseTitle
+    },
+    twitter: {
+      cardType: 'summary_large_image',
+      title: href !== '/' ? `${title} | ${baseTitle}` : baseTitle,
+      description: description,
+      image: `${PUBLIC_SITE_URL}/ogp.webp`,
+      imageAlt: 'OGP'
+    }
+  };
+</script>
+
+<MetaTags {...metaTags} />
