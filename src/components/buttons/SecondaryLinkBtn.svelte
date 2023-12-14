ed54c0d237e38ee4e2c909c9b81042ae891e7511
takara2314 <takara2314_TK@outlook.jp>
2023-12-15 00:42:45 +0900

    Improve: ボタンの装飾

--- src/components/buttons/SecondaryLinkBtn.svelte
+++ src/components/buttons/SecondaryLinkBtn.svelte
@@ -4,7 +4,10 @@
 
 <a
   href={href}
-  class="my-2 w-full h-14 text-white leading-[3.5rem] bg-neutral-400"
+  class="
+    my-2 w-full h-14 text-white leading-[3.5rem] bg-neutral-400
+    shadow-[inset_0.4rem_0.4rem_0.4rem_0_rgba(255,255,255,0.25)]
+  "
 >
   <slot />
 </a>
