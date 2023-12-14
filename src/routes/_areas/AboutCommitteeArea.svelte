49909560bfb203e555fa439180bc388ed4ff9c5d
takara2314 <takara2314_TK@outlook.jp>
2023-12-14 18:11:26 +0900

    Add: 実行委員会についてエリア

--- src/routes/_areas/AboutCommitteeArea.svelte
+++ src/routes/_areas/AboutCommitteeArea.svelte
@@ -1,7 +1,15 @@
-<section id="about-committee">
+<section
+  id="about-committee"
+  class="
+    mt-32 px-5 w-full h-full leading-8 text-center
+    flex flex-col items-center
+  "
+>
   <h2>
     志摩市二十歳の集い実行委員会について
   </h2>
 
-  テキスト
+  <p>
+    志摩市二十歳の集い実行委員会において、実行委員の皆様で式典の企画、運営に取り組んでいます。
+  </p>
 </section>
