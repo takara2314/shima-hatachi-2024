15545acd7d2ddb9cfb2d122e649a54682c9fac28
takara2314 <takara2314_TK@outlook.jp>
2023-12-14 20:53:57 +0900

    Add: フッター

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,3 +1,44 @@
-<footer>
-  © 2023-2024 志摩市二十歳の集い実行委員会
+<script>
+  import FooterItem from './FooterItem.svelte';
+</script>
+
+<footer
+  class="
+    mt-32 px-5 pt-8 pb-24 w-full bg-neutral-100
+    flex flex-col items-center
+    border-t-2 border-neutral-200
+  "
+>
+  <ul
+    class="w-full flex flex-col gap-5"
+  >
+    <FooterItem
+      name="しましまみえる Instagram"
+      description="「しましまみえる」とは、令和4年の成人式をきっかけに結成された、「志摩市に関わる全ての人たちのためのプラットフォーム」です。"
+      href="https://www.instagram.com/shima2mieru/"
+      iconSrc="/icons/shima2mieru.webp"
+      iconAlt="しましまみえる"
+    />
+    <FooterItem
+      name="生涯学習スポーツ課 Instagram"
+      description="志摩市に関する生涯学習と社会教育・文化・スポーツ等の情報を随時掲載していきます。"
+      href="https://www.instagram.com/shimacity_sgakuspo/"
+      iconSrc="/icons/shimacity_sgakuspo.webp"
+      iconAlt="生涯学習スポーツ課"
+    />
+  </ul>
+
+  <div class="mt-10">
+    <small class="text-gray-600">
+      © 2023-2024 志摩市二十歳の集い実行委員会
+    </small>
+  </div>
+  <div>
+    <small class="text-gray-600">
+      <span class="select-none">
+        協賛：
+      </span>
+      丸文工業㈱
+    </small>
+  </div>
 </footer>
---
+++ src/routes/FooterItem.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  export let name: string;
+  export let description: string;
+  export let href: string;
+  export let iconSrc: string;
+  export let iconAlt: string;
+</script>
+
+<li
+  class="bg-white border-2 border-yellow-600"
+>
+  <a
+    href={href}
+    class="p-3 w-full h-full flex flex-row gap-4"
+  >
+    <div
+      class="w-20 flex flex-col items-center justify-center"
+    >
+      <img
+        src={iconSrc}
+        alt={iconAlt}
+        class="w-full"
+      />
+    </div>
+
+    <div class="w-[calc(100%-5rem)]">
+      <h3 class="
+        text-sm
+        after:mb-2 after:content-['']
+        after:border-b-2 after:border-red-500 after:block
+      ">
+        {name}
+      </h3>
+      <p class="text-xs">
+        {description}
+      </p>
+    </div>
+  </a>
+</li>
