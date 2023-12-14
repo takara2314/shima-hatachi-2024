7d4f4e98f60851d8b5328bd492ae4ac86b6f239c
takara2314 <takara2314_TK@outlook.jp>
2023-12-15 00:11:44 +0900

    Add: イメージ画像を追加

--- src/routes/_areas/AboutCommitteeArea.svelte
+++ src/routes/_areas/AboutCommitteeArea.svelte
@@ -1,14 +1,36 @@
 <script>
   import Area from '../../components/Area.svelte';
   import BudouxP from '../../components/p/BudouxP.svelte';
+  import { asset } from '$lib/asset';
 </script>
 
-<Area id="about-committee">
-  <h2>
-    志摩市二十歳の集い実行委員会について
-  </h2>
+<Area
+  id="about-committee"
+  padding={false}
+>
+  <div class="px-4">
+    <h2>
+      志摩市二十歳の集い実行委員会について
+    </h2>
 
-  <BudouxP
-    text="志摩市二十歳の集い実行委員会において、実行委員の皆様で式典の企画、運営に取り組んでいます。"
+    <BudouxP
+      text="志摩市二十歳の集い実行委員会において、実行委員の皆様で式典の企画、運営に取り組んでいます。"
+    />
+  </div>
+
+  <img
+    src={asset('/photos/committee1.webp')}
+    alt="全員で会議 (オフライン)"
+    class="mt-10 w-full"
+  />
+  <img
+    src={asset('/photos/committee2.webp')}
+    alt="全員で会議 (Zoom)"
+    class="w-full"
+  />
+  <img
+    src={asset('/photos/committee3.webp')}
+    alt="恩師インタビューについて先生方に説明"
+    class="w-full"
   />
 </Area>
--- src/routes/_areas/ThemeArea.svelte
+++ src/routes/_areas/ThemeArea.svelte
@@ -1,36 +1,48 @@
 <script>
   import Area from '../../components/Area.svelte';
   import BudouxP from '../../components/p/BudouxP.svelte';
+  import { asset } from '$lib/asset';
 </script>
 
-<Area id="theme">
-  <h2>
-    テーマは「好天」
-  </h2>
+<Area
+  id="theme"
+  padding={false}
+>
+  <div class="px-4">
+    <h2>
+      テーマは「好天」
+    </h2>
 
-  <BudouxP
-    text="新型コロナウイルス感染症による影響も緩和され、今まで通りの日常が取りもどされつつあります。"
+    <BudouxP
+      text="新型コロナウイルス感染症による影響も緩和され、今まで通りの日常が取りもどされつつあります。"
+    />
+
+    <BudouxP
+      text="しかし、新型コロナウイルス感染症流行以前と同じ生活をするとなると、まだまだ曇り空が広がっています。"
+      class="mt-10"
+    />
+
+    <BudouxP
+      text="そこで我々の世代、学生から社会人まで二十歳になる全ての人が、 「二十歳の集い」をきっかけに曇り空を晴らそうという思いを込め"
+      class="mt-10"
+    />
+
+    <p>
+      「
+      <span class="text-red-500">
+        好天
+      </span>
+      」
+    </p>
+
+    <p>
+      というテーマをつけました。
+    </p>
+  </div>
+
+  <img
+    src={asset('/photos/koten.webp')}
+    alt="好天"
+    class="mt-10 w-full"
   />
-
-  <BudouxP
-    text="しかし、新型コロナウイルス感染症流行以前と同じ生活をするとなると、まだまだ曇り空が広がっています。"
-    class="mt-10"
-  />
-
-  <BudouxP
-    text="そこで我々の世代、学生から社会人まで二十歳になる全ての人が、 「二十歳の集い」をきっかけに曇り空を晴らそうという思いを込め"
-    class="mt-10"
-  />
-
-  <p>
-    「
-    <span class="text-red-500">
-      好天
-    </span>
-    」
-  </p>
-
-  <p>
-    というテーマをつけました。
-  </p>
 </Area>
