b25790003acc89f3c9a9e889a8a12891ae35d4c4
takara2314 <takara2314_TK@outlook.jp>
2023-12-14 17:59:21 +0900

    Add: 対象者エリア

--- src/routes/_areas/TargetArea.svelte
+++ src/routes/_areas/TargetArea.svelte
@@ -1,7 +1,18 @@
-<section id="target">
+<section
+  id="target"
+  class="
+    mt-32 px-5 w-full h-full leading-8 text-center
+    flex flex-col items-center
+  "
+>
   <h2>
     対象者
   </h2>
 
-  テキスト
+  <p>
+    平成15年（2003年）4月2日から
+  </p>
+  <p>
+    平成16年（2004年）4月1日生まれの人
+  </p>
 </section>
