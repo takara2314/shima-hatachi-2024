af6e695f2f6393b6795112c7a71869b000d7325f
takara2314 <takara2314_TK@outlook.jp>
2023-12-15 00:31:08 +0900

    Add: SNSボタン

--- src/routes/_areas/SnsShareArea.svelte
+++ src/routes/_areas/SnsShareArea.svelte
@@ -1,5 +1,7 @@
 <script>
   import Area from '../../components/Area.svelte';
+  import SnsButtonItem from './SnsButtonItem.svelte';
+  import { asset } from '$lib/asset';
 </script>
 
 <Area id="sns-share">
@@ -11,7 +13,7 @@
     式典の思い出をSNSに投稿しましょう！
   </p>
 
-  <p>
+  <p class="mt-10">
     投稿する際は、ハッシュタグ
     「
     <span class="text-red-500">
@@ -20,4 +22,24 @@
     」
     をつけてみんなと共有しましょう！
   </p>
+
+  <ul class="mt-5 flex flex-row gap-8">
+    <SnsButtonItem
+      href="https://x.com/search?q=%23%E5%BF%97%E6%91%A9%E5%B8%82%E4%BA%8C%E5%8D%81%E6%AD%B3%E3%81%AE%E9%9B%86%E3%81%842024"
+      iconSrc={asset('/icons/x.svg')}
+      iconAlt="X (旧Twitter)"
+    />
+
+    <SnsButtonItem
+      href="https://www.instagram.com/explore/tags/%E5%BF%97%E6%91%A9%E5%B8%82%E4%BA%8C%E5%8D%81%E6%AD%B3%E3%81%AE%E9%9B%86%E3%81%842024/"
+      iconSrc={asset('/icons/instagram.svg')}
+      iconAlt="Instagram"
+    />
+
+    <SnsButtonItem
+      href="https://www.tiktok.com/search?q=%23%E5%BF%97%E6%91%A9%E5%B8%82%E4%BA%8C%E5%8D%81%E6%AD%B3%E3%81%AE%E9%9B%86%E3%81%842024"
+      iconSrc={asset('/icons/tiktok.svg')}
+      iconAlt="TikTok"
+    />
+  </ul>
 </Area>
