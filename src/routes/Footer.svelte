e7ebfe0287de30db41dcdc34882adfdacf5ec72c
takara2314 <takara2314_TK@outlook.jp>
2024-01-04 09:19:14 +0900

    Refactor: サポーター情報は配列で管理するように

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,6 +1,23 @@
 <script>
   import { asset } from '$lib/asset';
   import FooterItem from './FooterItem.svelte';
+
+  const supporters = [
+    {
+      name: 'しましまみえる Instagram',
+      description: '「しましまみえる」とは、令和4年の成人式をきっかけに結成された、「志摩市に関わる全ての人たちのためのプラットフォーム」です。',
+      href: 'https://www.instagram.com/shima2mieru/',
+      iconSrc: asset('/icons/shima2mieru.webp'),
+      iconAlt: 'しましまみえる'
+    },
+    {
+      name: '生涯学習スポーツ課 Instagram',
+      description: '志摩市に関する生涯学習と社会教育・文化・スポーツ等の情報を随時掲載していきます。',
+      href: 'https://www.instagram.com/shimacity_sgakuspo/',
+      iconSrc: asset('/icons/shimacity_sgakuspo.webp'),
+      iconAlt: '生涯学習スポーツ課'
+    }
+  ];
 </script>
 
 <footer
@@ -14,20 +31,11 @@
   <ul
     class="w-full flex flex-col gap-5"
   >
-    <FooterItem
-      name="しましまみえる Instagram"
-      description="「しましまみえる」とは、令和4年の成人式をきっかけに結成された、「志摩市に関わる全ての人たちのためのプラットフォーム」です。"
-      href="https://www.instagram.com/shima2mieru/"
-      iconSrc={asset('/icons/shima2mieru.webp')}
-      iconAlt="しましまみえる"
-    />
-    <FooterItem
-      name="生涯学習スポーツ課 Instagram"
-      description="志摩市に関する生涯学習と社会教育・文化・スポーツ等の情報を随時掲載していきます。"
-      href="https://www.instagram.com/shimacity_sgakuspo/"
-      iconSrc={asset('/icons/shimacity_sgakuspo.webp')}
-      iconAlt="生涯学習スポーツ課"
-    />
+    {#each supporters as supporter}
+      <FooterItem
+        {...supporter}
+      />
+    {/each}
   </ul>
 
   <div class="mt-10">
